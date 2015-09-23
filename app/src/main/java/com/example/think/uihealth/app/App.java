8f9d961181425f901cd9a3e4138e43e777b79aa5
Zane <709369487@qq.com>
2015-09-23 23:45:37 +0800

    完成view

--- app/src/main/java/com/example/think/uihealth/app/App.java
+++ app/src/main/java/com/example/think/uihealth/app/App.java
@@ -24,5 +24,6 @@ public class App extends Application {
     public static Context getInstance() {
 
         return instance;
+
     }
 }
---
+++ app/src/main/java/com/example/think/uihealth/view/adapter/ContentViewpagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.think.uihealth.view.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.think.uihealth.view.fragment.ContentItemFragment;
+
+import java.util.List;
+
+/**
+ * Created by think on 2015/9/22.
+ */
+public class ContentViewpagerAdapter extends FragmentStatePagerAdapter{
+
+    private List<ContentItemFragment> mFragments;
+
+    public ContentViewpagerAdapter(FragmentManager fm, List<ContentItemFragment> mFragments) {
+        super(fm);
+        this.mFragments = mFragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        return this.mFragments.get(position);
+
+    }
+
+    @Override
+    public int getCount() {
+
+        return this.mFragments.size();
+
+    }
+}
