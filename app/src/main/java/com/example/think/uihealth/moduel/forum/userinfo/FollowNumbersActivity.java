c700f28ebe11d97b3bd1ee6af6b1a69e56cdb0aa
徐志 <wo1996zhi@vip.qq.com>
2015-11-17 21:03:01 +0800

    init

---
+++ app/src/main/java/com/example/think/uihealth/moduel/forum/userinfo/FollowNumbersActivity.java
@@ -0,0 +1,21 @@
+package com.example.think.uihealth.moduel.forum.userinfo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zane on 15/11/17.
+ */
+public class FollowNumbersActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
